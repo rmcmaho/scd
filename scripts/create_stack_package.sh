be62492f3e85b22801979781ff763874e1a8a62e
rmcmahon <mcmahon.robbie.spam@gmail.com>
2017-12-30 16:45:14 -0800

    Package script detect python version. Updated travis build to 3.6 because it is the default. Need to detect python because Ubuntu 16.04 uses python 3.5.

--- scripts/create_stack_package.sh
+++ scripts/create_stack_package.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -x
 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
+# Any subsequent commands which fail will cause the script to exit.
+set -e
+
+PYTHON_VERSION=$(python -c 'import sys; print(sys.version_info[:])')
+read PYTHON_MAJOR PYTHON_MINOR <<< $(echo $PYTHON_VERSION | awk -F',' '{print substr($1,2) $2}')
+PYTHON_DIST=python$PYTHON_MAJOR.$PYTHON_MINOR
+
+# Absolute path to this script
 SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
+# Absolute path to script dir
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
 ROOT_DIR=$(readlink -f "$SCRIPT_DIR/..")
@@ -15,7 +22,7 @@ mkdir -p $BUILD_DIR
 mkdir -p $SHARED_DIR
 
 rsync -a $SRC_DIR/python/ $BUILD_DIR/python
-rsync -a $VIRTUAL_ENV/lib/python3.5/site-packages/ $BUILD_DIR/python
+rsync -a $VIRTUAL_ENV/lib/$PYTHON_DIST/site-packages/ $BUILD_DIR/python
 rsync -a $CONFIG_DIR/stellar_stack.yml $BUILD_DIR/stellar_stack.yml
 
 aws cloudformation package --template-file $BUILD_DIR/stellar_stack.yml --output-template-file $SHARED_DIR/stellar_stack.yml --s3-bucket scd-code
