f6cb4ddfc4022f611aa0bff085374bb42f0b40dc
rmcmahon <mcmahon.robbie.spam@gmail.com>
2017-12-30 20:45:27 -0800

    Lambda and GatewayAPI for stellar code. Allows uploading stars to the dynamodb table.

---
+++ src/python/stellar_code_function.py
@@ -0,0 +1,64 @@
+
+import decimal
+import json
+
+import boto3
+
+import stellar_code.stellar_code as stellar_code
+
+
+TABLE_STELLAR_SYSTEM_ = 'stellar_systems'
+TABLE_STELLAR_SECTORS_ = 'stellar_sectors'
+TABLE_SYSTEM_SEARCHES_ = 'system_searches'
+
+
+def generate_system_name(req):
+    if 'proper' in req:
+        return req['proper']
+    if 'gl' in req:
+        return req['gl']
+    if 'hip' in req:
+        return 'HIP ' + req['hip']
+    return 'HYG ' + req['hyg']
+
+
+def insert_stellar_system(db_client, req_json):
+    request = json.loads(req_json)
+    sector_id = stellar_code.encode_equitorial(request['ra'], request['dec'], request['dist'])
+    system_name = generate_system_name(request)
+    item = {        
+        'sector_id': sector_id,
+        'system_name': system_name
+    }
+    table_client = db_client.Table(TABLE_STELLAR_SYSTEMS_)
+    return table_client.put_item(Item=item)
+
+
+def route_event(resource_path, http_method):
+    operations = {
+        '/stellar/code' : { 'POST' : insert_stellar_system}
+    }
+
+    if resource_path not in operations:
+        raise ValueError('Unsupported path "{}"'.format(resource_path))
+
+    resource = operations[resource_path]
+    if http_method not in resource:
+        raise ValueError('{0} not supported on "{1}"'.format(http_method, resource))
+
+    return resource[http_method]
+
+
+def handler(event, context):
+
+    print("Received event: " + json.dumps(event, indent=2))
+
+    operation = route_event(context.resourcePath, context.httpMethod)
+    response = operation(boto3.resource('dynamodb'), event)
+    
+    print("Response: " + response)
+    return response
+    
+    
+
+
