6469560c37a678150bd9b21e2f3cd83eb5219048
rmcmahon <mcmahon.robbie.spam@gmail.com>
2018-01-07 20:43:12 -0800

    Resolve template errors. Add missing elements. Add dependencies. Fix typos.

--- scripts/setup_stack.py
+++ scripts/setup_stack.py
@@ -49,10 +49,12 @@ def virtualenv_site_packages():
 
 CONFIG_STACK_NAME_ = 'scd-config'
 STACK_NAME_ = 'scd'
+IAM_STACK_ = 'iam-stack'
 
 STACK_TEMPLATE_PATH_ = {
     CONFIG_STACK_NAME_ : config_stack_template_path(),
-    STACK_NAME_ : root_stack_template_path()
+    STACK_NAME_ : root_stack_template_path(),
+    IAM_STACK_ : config_file('users_and_roles.yml')
 }
 
 
@@ -96,7 +98,8 @@ def create_stack(cloud_client, stack_name):
     return stack
 
 
-def create_change_set(cloud_client, stack_name):
+def create_change_set(cloud_client, stack_name, parameters=[]):
+    print('Creating change set for ' + stack_name)
     template_path = STACK_TEMPLATE_PATH_[stack_name]
     with open(template_path, 'r') as f:
         stack_template = f.read()
@@ -104,9 +107,10 @@ def create_change_set(cloud_client, stack_name):
     response = cloud_client.create_change_set(
         StackName=stack_name,
         TemplateBody=stack_template,
-        Capabilities=['CAPABILITY_IAM'],
+        Capabilities=['CAPABILITY_NAMED_IAM'],
         ChangeSetName=stack_name+'-CHANGESET',
-        ChangeSetType='CREATE'
+        ChangeSetType='CREATE',
+        Parameters=parameters
     )
 
     change_set_arn = response['Id']
@@ -116,14 +120,21 @@ def find_in_outputs(stack_desc, key):
     return next(e['OutputValue'] for e in stack_desc['Outputs'] if e['OutputKey'] == key)
 
 
-def upload_config_file(s3_client, bucket_name, template_name):
-    with open(config_file(template_name), 'rb') as f:
+def upload_file(s3_client, bucket_name, file_path, file_key=None):
+    if not file_key:
+        file_key = os.path.basename(file_path)
+    print('Uploading {0} --> {1}/{2}'.format(file_path, bucket_name, file_key))
+    with open(file_path, 'rb') as f:
         response = s3_client.put_object(
             Body=f,
             Bucket=bucket_name,
-            Key=template_name
+            Key=file_key
         )
     return response['VersionId']
+
+
+def upload_config_file(s3_client, bucket_name, template_name):
+    return upload_file(s3_client, bucket_name, config_file(template_name))
 
 
 def excluded_by(text, excludes=[]):
@@ -133,53 +144,27 @@ def excluded_by(text, excludes=[]):
 def package_add_dir(package, src, excludes=[]):
     for root, dirs, files in os.walk(src):
         rel_path = os.path.relpath(root, src)
-        
         dirs[:] = [d for d in dirs if not excluded_by(d, excludes)]
-        #for d in dirs:
-        #    package.write(os.path.join(rel_path, d))
-        
         files[:] = [f for f in files if not excluded_by(f, excludes)]
         for f in files:
             src_file = os.path.join(root, f)
             dst_file = os.path.join(rel_path, f)
-            # print(src_file + ' --> ' + dst_file)
             package.write(src_file, arcname=dst_file)
 
 
 def create_package(src_list, dst, excludes=[]):
     with zipfile.ZipFile(dst, 'w', compression=zipfile.ZIP_DEFLATED) as package:
         for src in src_list:
-            print(src)
+            print('Zipping ' + src + ' --> ' + dst)
             package_add_dir(package, src, excludes)
 
 
-def main():
-    cloud_client = boto3.client('cloudformation')
-    try:
-        response = cloud_client.describe_stacks(StackName=CONFIG_STACK_NAME_)
-        config_desc = next(iter(response['Stacks']), None)
-    except botocore.exceptions.ClientError:
-        config_desc = None
-
-    if config_desc is None:
-        config_desc = create_stack(cloud_client, CONFIG_STACK_NAME_)
-
-    print(dump_cf_response(config_desc))
-
-    bucket_name = find_in_outputs(config_desc, 'ConfigS3BucketName')
-
-    s3_client = boto3.client('s3')
-    iam_template_version = upload_config_file(s3_client, bucket_name, 'users_and_roles.yml')
-    stellar_api_template_version = upload_config_file(s3_client, bucket_name, 'stellar_api.yml')
-    
-    # print("Creating change set")
-    # create_change_set(cloud_client, STACK_NAME_)
-
-    python_srcs = [
+def create_python_lambda_package():
+    srcs = [
         os.path.join(root_dir(), 'src', 'python'),
         virtualenv_site_packages()
     ]
-    python_excludes = [
+    excludes = [
         '^awscli',
         '^docutils',
         '^python_dateutils',
@@ -192,14 +177,31 @@ def main():
     ]
 
     package_dst = os.path.join(root_dir(), 'build', 'python_package.zip')
-    create_package(python_srcs, package_dst, python_excludes)
+    create_package(srcs, package_dst, excludes)
+    return package_dst
+
+
+def main():
+    cloud_client = boto3.client('cloudformation')
+    try:
+        response = cloud_client.describe_stacks(StackName=CONFIG_STACK_NAME_)
+        config_desc = next(iter(response['Stacks']), None)
+    except botocore.exceptions.ClientError:
+        config_desc = None
+
+    if config_desc is None:
+        print('Creating config stack')
+        config_desc = create_stack(cloud_client, CONFIG_STACK_NAME_)
+
+    bucket_name = find_in_outputs(config_desc, 'ConfigS3BucketName')
+
+    s3_client = boto3.client('s3')
+    iam_template_version = upload_config_file(s3_client, bucket_name, 'users_and_roles.yml')
+    stellar_api_template_version = upload_config_file(s3_client, bucket_name, 'stellar_api.yml')
     
-    print("Uploading package")
-    with open(package_dst, 'rb') as f:
-        lambda_package_resp = s3_client.put_object(
-            Body=f,
-            Bucket=bucket_name,
-            Key='lambda_package.zip'
-        )
+    python_package = create_python_lambda_package()
+    python_package_version = upload_file(s3_client, bucket_name, python_package, 'lambda_package.zip')
+
+    # create_change_set(cloud_client, STACK_NAME_)
 
 main()
